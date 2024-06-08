8686e814b8f66b6d31edbdb9289ea7bf4a0ff3e6
G1djet <gadjet.mot@mail.ru>
2024-06-08 19:33:13 +0700

    1

--- z3/build/cmake_install.cmake
+++ z3/build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/tpu/programming/lab1/1.3
+# Install script for directory: D:/tpu/programming/lab1/z3
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/1.3")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/z3")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/tpu/programming/lab1/1.3/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/tpu/programming/lab1/z3/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
